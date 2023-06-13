79f76c49ea98b82bd5195815d6a370191b9a7d86
BreAnna Baker <124074031+brennybaker@users.noreply.github.com>
2023-06-13 01:04:54 +0000

    updated signup and login routes to be barebones/functional without bcrypt for now

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,48 +1,50 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, session
 from api.models import db, User, Post, Favorites
-from api.utils import generate_sitemap, APIException
-from werkzeug.security import generate_password_hash
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
 
 api = Blueprint('api', __name__)
 app = Flask(__name__)
 
-@app.route('/register', methods=['POST'])
-def signup_user():
-    data = request.get_json()
 
-    hashed_password = generate_password_hash(data['password'], method='sha256')
+@api.route('/signup', methods=['POST'])
+def signup():
+    # Retrieve request data
+    username = request.json.get('username')
+    password = request.json.get('password')
 
-    new_user = User(username=data['username'], password=hashed_password)
-    
-    # checking if that user already exists
-    existing_user = User.query.filter_by(username=data['username']).first()
-    if existing_user:
-        return jsonify({'message': 'User already exists!'}), 409  # HTTP status code for conflict
+    # Check if the email is already registered
+    if User.query.filter_by(username=username).first():
+        return jsonify(message='Username already registered'), 200
 
-    db.session.add(new_user)
-    db.session.commit()
+    # Create a new user object
+    new_user = User(username=username, password=password)
 
-    return jsonify({'message': 'registered successfully'}), 201  # HTTP status code for created
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(message='Failed to register user'), 200
+
+    return jsonify(message='User registered successfully'), 200
 
 
-@app.route('/login', methods=['POST'])
-def login_user(): 
-    auth = request.authorization
-    if not auth or not auth.username or not auth.password:  
-        return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})    
-    
-    user = next((x for x in users if x.username == auth.username), None)
-    if not user:
-        return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
-    
-    if check_password_hash(user.password, auth.password):  
-        token = jwt.encode({'public_id': user.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-        return jsonify({'token' : token.decode('UTF-8')})
+@api.route('/login', methods=['POST'])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
 
-    return make_response('could not verify',  401, {'WWW.Authentication': 'Basic realm: "login required"'})
+    # Perform authentication
+    user = User.query.filter_by(username=username).first()
+    if user is None or not password == user.password:
+        return jsonify({"msg": "Incorrect email or password"}), 401
+
+    # Generate access token
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
 
 
 @api.route('/hello', methods=['POST', 'GET'])
@@ -58,8 +60,9 @@ def handle_hello():
 @api.route('/users/<int:id>', methods=['GET'])
 def get_user_(id):
     user = User.query.filter_by(id=id).first()
-    #serialized_users = [user.serialize() for user in user]
+    # serialized_users = [user.serialize() for user in user]
     return jsonify(user.serialize())
+
 
 @api.route('/users/favorites', methods=['POST'])
 def add_favorite():
@@ -68,10 +71,11 @@ def add_favorite():
         user_id=data['user_id'],
         post_id=data['post_id'],
     )
-    
+
     db.session.add(favorite)
     db.session.commit()
     return "SUCCESS"
+
 
 @api.route('/users/favorites/<int:id>', methods=['DELETE'])
 def delete_favorite(id):
@@ -81,3 +85,24 @@ def delete_favorite(id):
         db.session.commit()
         return "SUCCESS"
     return "Favorite not found"
+
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user_username = get_jwt_identity()
+    user = User.query.filter_by(username=current_user_username).first()
+
+    request_token = request.headers.get('Authorization')
+
+    if request_token and request_token.startswith('Bearer '):
+        stored_token = request_token.split('Bearer ')[1]
+
+    if stored_token == session.get('access_token'):
+        return jsonify({"username": user.username}), 200
+    else:
+        return jsonify(message='You are not authorized to view this page'), 401
+
+
+if __name__ == "__main__":
+    api.run()
