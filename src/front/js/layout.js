4188c3965cd4aff59d65113d204fac0ab036de5c
EdgarDMen <123202799+EdgarDMen@users.noreply.github.com>
2023-06-19 17:31:59 +0000

    Home page and images with AWS storage (route will be completed soon after)

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -26,8 +26,7 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Login />} path="/login" />
+                        <Route element={<Login />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,43 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
+ function App() {
+     	//image URLs from AWS
+const images = [
+    'https://s3://storingaigoraphotos/cittaVecchia.jpg',
+    'https://s3://storingaigoraphotos/unTigreDiaoulo.jpeg',
+    'https://s3://storingaigoraphotos/animeProtag.jpeg',
+    'https://s3://storingaigoraphotos/degas.jpg',
+    'https://s3://storingaigoraphotos/elVagabondoPecador.jpg',
+    'https://s3://storingaigoraphotos/greekIcon.jpg',
+    'https://s3://storingaigoraphotos/Dali.jpg',
+    'https://s3://storingaigoraphotos/Faro.jpg',
+    'https://s3://storingaigoraphotos/SorElena.jpg',
+    'https://s3://storingaigoraphotos/Rembrandt.jpg',
+    'https://s3://storingaigoraphotos/Africa.jpg',
+    'https://s3://storingaigoraphotos/OVNI.jpeg',
+    'https://s3://storingaigoraphotos/ZhongGuo.jpg',
+    'https://s3://storingaigoraphotos/sentientUniverse.jpg',
+    'https://s3://storingaigoraphotos/compasion.jpg',
+    'https://s3://storingaigoraphotos/laRagazzaDiSoffrenza.jpg',
+	];
+  
+  return (
+ 	  <Container>
+ 		{images.map((image, idx) => (
+ 		  <Row key={idx}>
+ 			<Col xs={12} md={6} lg={4}>
+ 			  <img src={image} className="img-fluid" alt="Responsive" />
+ 			</Col>
+ 		  </Row>
+ 		))}
+ 	  </Container>
+ 	);
+  }
+  
+  export default App;
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
-};
